d4a42efdfec20eec3c4d6346a70cf9af33b4bce8
shuchi54 <shuchis352@gmail.com>
2021-08-22 23:24:30 +0530

    add signin

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,18 @@
 module.exports.profile = function(req,res){
   res.end('<h1>User profile</h1>');
 }
+
+//render the sign up page
+module.exports.signUp = function(req,res){
+  return res.render('user_sign_up', {
+    title : "Allsocial | Sign Up"
+  })
+}
+
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+  return res.render('user_sign_in', {
+    title: "Allsocial | Sign Up"
+  })
+}
